9e700282364108e1cbbc985edc727c063bd49537
tminhat13 <83794482+tminhat13@users.noreply.github.com>
2023-10-14 10:21:54 -0700

    init program

---
+++ src/recursive/rebels/pkg3d/world/RecursiveRebels3DWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package recursive.rebels.pkg3d.world;
+
+/**
+ *
+ * @author prapi
+ */
+public class RecursiveRebels3DWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
